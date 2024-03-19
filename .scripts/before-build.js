344f79f05796bca8d86791f469f8d26f80aeb176
thepoy <thepoy@163.com>
2024-03-19 21:12:29 +0800

    chore(workflows): 更新工作流配置

---
+++ .scripts/before-build.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+const updateVersion = () => {
+  const infoBuffer = fs.readFileSync('../package.json')
+  const info = JSON.parse(infoBuffer.toString())
+
+  const { version } = info
+
+  if (process.platform !== 'win32') {
+    return version
+  }
+
+  if (version.indexOf('-') === -1) {
+    return
+  }
+
+  const newVersion = version.split('-')[0]
+  info.version = newVersion
+
+  fs.writeFileSync('../package.json', JSON.stringify(info))
+
+  return newVersion
+}
+
+console.log(updateVersion())
